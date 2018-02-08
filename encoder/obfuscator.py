ff859b0ef376e53d21f3423c9527e69d266c1ace
Tasos Chatz <element0002002@gmail.com>
2018-02-08 19:34:03 +0200

    Update obfuscator.py

--- encoder/obfuscator.py
+++ encoder/obfuscator.py
@@ -3,6 +3,12 @@
 import urllib
 import os
 import binascii
+import base64
+
+class bcolors:
+	OKGREEN = '\033[92m'
+	BOLD = '\033[1m'
+	ENDC = '\033[0m'
 
 #Encoder
 #Read Coders from file
@@ -14,7 +20,13 @@ s=raw_input("Enter a string: ")
 #Encoding Types
 
 for x in f:
-	try:
+	try:	
+		if not tags.has_key('base16'):
+			tags.update({'base16': base64.b16encode(s)})
+
+		if not tags.has_key('base32'):
+                        tags.update({'base32': base64.b32encode(s)})
+	
 		if not tags.has_key('quote_plus'):
 			tags.update({'quote_plus': urllib.quote_plus(s)})
 
@@ -80,26 +92,59 @@ for item in tags.items():
 		unique_dict.update({y:x})
 
 #stdout unique values
-counter=0
+#print bcolors.OKGREEN + bcolors.BOLD + "PYTHON!!!" + bcolors.ENDC
+prefix=bcolors.OKGREEN + bcolors.BOLD+"[+]"+bcolors.ENDC
 for item2 in unique_dict.items():
 	m,n=item2
 	if hasattr(n,'strip') and hasattr(m,'strip'):
-		print str(counter)+") %s -> %s" % (n.strip('\n'),m.strip('\n'))
+		print str(prefix)+" %s -> %s" % (n.strip('\n'),m.strip('\n'))
 	elif n == "unicode_int":
-		print str(counter)+") %s -> String.fromCharCode%s  //Bypass Javascript filters" % (n,str(m))
+		print str(prefix)+" %s -> String.fromCharCode%s  //Bypass Javascript filters" % (n,str(m))
 	elif n == "unicode_hex":
 		#Add dec values with &#x; to oneline_html_hex
 		#Add dec values with &#; to oneline_html_dec
 		oneline_html_hex=[]
 		oneline_html_dec=[]
+		oneline_char_mssql=[]
+                oneline_char_oracle=[]
+                oneline_char_mysql=[]
+		oneline_char2_mysql=[]
+		oneline_ascii_mysql=[]
 		for x in m:
 			oneline_html_hex.append("&#x"+str(x)+";")
 			oneline_html_dec.append("&#"+str(ord(binascii.unhexlify(x)))+";")
-		print str(counter)+") %s -> %s  //Bypass HTML filters" % (n,''.join(oneline_html_hex))
+			oneline_char_mssql.append("char("+str(ord(binascii.unhexlify(x)))+")+")
+                	oneline_char_oracle.append("char("+str(ord(binascii.unhexlify(x)))+")||")
+                	oneline_char_mysql.append(str(ord(binascii.unhexlify(x)))+",")
+			oneline_char2_mysql.append("char("+str(ord(binascii.unhexlify(x)))+"),")
+			oneline_ascii_mysql.append("ascii("+str(ord(binascii.unhexlify(x)))+"),")
+		print str(prefix)+" %s -> %s  //Bypass HTML filters" % (n,''.join(oneline_html_hex))
+		#MSsql CHAR()
+		#Strip the latest +
+		latest_plus_index=str(''.join(oneline_char_mssql)).rindex("+")
+		strip_plus=str(''.join(oneline_char_mssql)).strip(''.join(str(''.join(oneline_char_mssql))[latest_plus_index]))
+		print str(prefix)+" %s -> %s  //Bypass MSSQL filters" % ('mssql',strip_plus)
+		#Oracle CHAR()
+		#Strip the latest ||
+		latest_or_index=str(''.join(oneline_char_oracle)).rindex("||")
+		strip_or=str(''.join(oneline_char_oracle)).strip(''.join(str(''.join(oneline_char_oracle))[latest_or_index]))
+		print str(prefix)+" %s -> %s  //Bypass Oracle filters" % ('oracle',strip_or)
+		#MySQL CHAR()
+		#Strip the latest ,
+		latest_comma_index=str(''.join(oneline_char_mysql)).rindex(",")
+		strip_comma=str(''.join(oneline_char_mysql)).strip(''.join(str(''.join(oneline_char_mysql))[latest_comma_index]))
+		print str(prefix)+" %s -> char(%s)  //Bypass MySQL filters" % ('mysql',strip_comma)
+		#MySQL(2) CHAR
+		latest_comma_index=str(''.join(oneline_char2_mysql)).rindex(",")
+		strip_comma=str(''.join(oneline_char2_mysql)).strip(''.join(str(''.join(oneline_char2_mysql))[latest_comma_index]))
+		print str(prefix)+" %s -> %s  //Bypass MySQL filters (2)" % ('mysql(2)',strip_comma)
+		#MySQL(3) ASCII
+		latest_comma_index=str(''.join(oneline_ascii_mysql)).rindex(",")
+                strip_comma=str(''.join(oneline_ascii_mysql)).strip(''.join(str(''.join(oneline_ascii_mysql))[latest_comma_index]))
+                print str(prefix)+" %s -> %s  //Bypass MySQL filters (3)" % ('mysql(3)',strip_comma)
 		#Convert hex to dec
-		print str(counter)+") unicode_dec -> %s  //Bypass HTML filters" % (''.join(oneline_html_dec))
-	counter=counter+1
-		
+		print str(prefix)+" unicode_dec -> %s  //Bypass HTML filters" % (''.join(oneline_html_dec))
+
 
 
 f.close()
